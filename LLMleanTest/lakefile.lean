a26842dc8e1ae916358ea8c64ec2f0220593dd6e
Sean Welleck <wellecks@gmail.com>
2025-10-10 15:26:28 -0400

    Lean 4.23.0

--- LLMleanTest/lakefile.lean
+++ LLMleanTest/lakefile.lean
@@ -5,7 +5,7 @@ package LLMleanTest
 
 require llmlean from ".."
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.20.0-rc5"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.23.0"
 
 @[default_target]
 lean_lib LLMleanTest
